10c4c1b90ddbcdcd8cf18507b80fe23e480febed
imisi99 <isongrichard234@gmail.com>
2025-02-02 23:13:04 +0100

    adding streamlit for frontend

---
+++ frontend/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import time
+
+# st.title("AltSchool Spotify API")
+# val ="Welcome Imisioluwa to a whole new world"
+#
+#
+# lc, rc = st.columns(2)
+#
+# val
+# x = st.sidebar.slider('squared')
+# st.write(x, 'squared is', x * x)
+#
+# lc.text_input("Enter your name", key="username")
+#
+# if st.sidebar.checkbox('Display name'):
+#     st.session_state.username
+#
+# cars = ['cyber_truck', 's model']
+#
+# option = st.sidebar.selectbox('Choose a car', cars)
+#
+# 'You selected: ', option
+#
+#
+# lc.button('Press me!')
+# with rc:
+#     chosen = st.radio('Which car?', cars)
+#     st.write(f"you selected: {chosen}")
+
+'Starting a long computation'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f"Progress {i+1}")
+    bar.progress(1 + i)
+    time.sleep(0.5)
+
+'done'
