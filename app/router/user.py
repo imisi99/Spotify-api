93fe848041af7378c9b7e6ba094fc73a27ec3cd7
imisi99 <isongrichard234@gmail.com>
2025-02-16 22:51:36 +0100

    setting cookies

--- app/router/user.py
+++ app/router/user.py
@@ -132,6 +132,7 @@ def callback(request: Request, db: db_dependency):
             key='refresh_token',
             value=refresh_token,
             max_age=60 * 60 * 24 * 30,
+            domain="dashie23.streamlit.app",
             httponly=False,
             secure=True,
             samesite="none"
