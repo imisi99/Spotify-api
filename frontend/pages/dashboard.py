47abc5340a8cb0364098d4ab54136260caee9b19
imisi99 <isongrichard234@gmail.com>
2025-02-16 16:56:18 +0100

    adding profile to frontend

--- frontend/pages/dashboard.py
+++ frontend/pages/dashboard.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import requests
 
-p_uri = "https://spotify-dv92.onrender.com/user/get_cookies"
+p_uri = "https://spotify-dv92.onrender.com/user/profile"
 
 session = requests.Session()
 response = session.get(p_uri)
-st.link_button('get_cookie', p_uri)
 st.write(response.json())
